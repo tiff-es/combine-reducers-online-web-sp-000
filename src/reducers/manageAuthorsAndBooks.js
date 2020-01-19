2dd8b79ab7560c29083ea8cda4199cc80966a5e7
boostinwrx <tony.calablava@gmail.com>
2020-01-18 19:52:51 -0500

    Done.

--- src/reducers/manageAuthorsAndBooks.js
+++ src/reducers/manageAuthorsAndBooks.js
@@ -1,37 +1,35 @@
-export default function bookApp(
-  state = {
-    authors: [],
-    books: []
-  },
-  action
-) {
-  let idx;
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+  authors: authorsReducer,
+  books: booksReducer
+})
+export default rootReducer
+
+const booksReducer = (state = [], action)
+{
+  let idx
   switch (action.type) {
-    case "ADD_BOOK":
-      return {
-        ...state,
-        books: [...state.books, action.book]
-      };
-
+    case 'ADD_BOOK':
+      return [...state, action.book]
     case "REMOVE_BOOK":
-      idx = state.books.findIndex(book => book.id === action.id);
-      return {
-        ...state,
-        books: [...state.books.slice(0, idx), ...state.books.slice(idx + 1)]
-      };
+      idx = state.findIndex(book => book.id === action.id)
+      return [...state.slice(0, idx), ...state.slice(idx + 1)];
 
+    default:
+      return state;
+  }
+
+}
+
+const authorsReducer = (state = [], action){
+  switch (action.type){
     case "ADD_AUTHOR":
-      return {
-        ...state,
-        authors: [...state.authors, action.author]
-      };
+      return [...state, action.author];
 
     case "REMOVE_AUTHOR":
-      idx = state.authors.findIndex(author => author.id === action.id);
-      return {
-        ...state,
-        authors: [...state.authors.slice(0, idx), ...state.authors.slice(idx + 1)]
-      };
+      idx = state.findIndex(author => author.id  === action.id)
+      return [...state.slice(0, idx), ...state.slice(idx + 1)];
 
     default:
       return state;
